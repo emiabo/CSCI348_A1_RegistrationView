7e4fe4b4ffab425f95fc565250ea5c52769bd814
Emilia Aboudara <naboudara@gmail.com>
2022-10-13 10:04:05 -0700

    Initial commit

---
+++ register.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/styles.css">
+        <title>Login</title>
+        <meta name="author" content="Emi Aboudara">
+        <meta name="keywords" content="">
+    </head>
+    <body>
+        
+    </body>
+</html>
